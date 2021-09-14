e114cb9db28bf7267c7f3116e4257276b8aa6580
Joe <jmcook1186@gmail.com>
2021-09-14 15:49:00 +0100

    add funcs to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,193 @@
 import React, {useState} from 'react';
 import "./App.css";
 import Web3 from 'web3'
-import BrightLink from '/home/joe/Code/BrightLink/client/src/artifacts/contracts/BrightLink.json';
-const BrightLinkAddress = '0xa442852B9B9A92B277E6aF0FDcAe986EAa1986B3'
+import background from "./SolarPunkPainting.jpg";
+import BrightLink from './artifacts/contracts/BrightLink.json';
+import map from "./artifacts/deployments/map.json"
 
+// uses functional component syntax
+function App(){
 
-
-function App (){
-
-const [account, setOwnerAccount] = useState()
-const [customerAddress, setCustomerAddress] = useState()
-const [donorAddress, setDonorAddress] = useState()
-const [agreementValue, setAgreementValue] = useState()
-const [network, setNetwork] = useState()
-var connected = false;
-
-async function loadBlockchainData(){
-  
+  // web3 is whatever is injected by metamask, else localhost
+  // add deployed address for Solidity Storage contract
   const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
-  const network = await web3.eth.net.getNetworkType()
-  console.log("You are connected to the **", network, "** network")
-  const accounts = await web3.eth.getAccounts()
-  setOwnerAccount(accounts[0])
-  console.log("account = ", account)
-  setNetwork({network})
-  connected = true
-}
-
-
-async function AddNewCustomer(){
+  // define vars and functions to update them, associated to react state
   
-  const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
-  const contract = new web3.eth.Contract(BrightLink.abi, BrightLinkAddress, account)
+  const [account, setOwnerAccount] = useState()
+  const [chainID, setChainID] = useState()
+  const [network, setNetwork] = useState()
+  const [contractAddress, setContractAddress] = useState()
+  const [contract, setContract] = useState()
+  const[customerAddress, setCustomerAddress] = useState()
+  const[donorAddress, setDonorAddress] = useState()
+  const [agreementValue, setAgreementValue] = useState()
+  const[weight1, setWeight1] = useState()
+  const[weight2, setWeight2] = useState()
+  const[weight3, setWeight3] = useState()
+  const [connected, connect] = useState()
+
+
+  async function loadBlockChain(){
+    
+    const chain = await web3.eth.getChainId()
+    const net = await web3.eth.net.getNetworkType()
+    const accounts = await web3.eth.getAccounts()
+    const acc = accounts[0]
+
+    setNetwork(net)
+    setChainID(chain)
+    setOwnerAccount(acc)
+
+    console.log(chain)
+    var address = ''
+
+    if (chain =='1337'){
+      address = map["dev"]['BrightLink'][0]
+    }
+    else if(chain == 42){
+      address = map["42"]['BrightLink'][0]
+    }
+    else if(chain==3){
+      address = map["3"]['BrightLink'][0]
+    }
+    else if(chain==4){
+       address = map["4"]['BrightLink'][0]
+    }
+    else{
+      throw 'Please connect to a valid testnet'
+    }
+
+    setContractAddress(address)
+    const _contract = new web3.eth.Contract(BrightLink.abi, address, account)
+
+    setContract(_contract)
+    console.log(address)
+    console.log(_contract)
+    console.log(chain)
+    console.log("contract successfully loaded")
+    
+    setWeight1(100)
+    setWeight2(100)
+    setWeight3(100)
+    connect(true)
+  }
+
+
+
+  async function AddNewCustomer(){
+   
+    await contract.methods.addNewCustomer(customerAddress,donorAddress,agreementValue)
+      .send({'from':account})
+        .then(console.log("successfully added new customer"))
+    
+    console.log(customerAddress)
+    console.log(donorAddress)
+    console.log(agreementValue)
+    console.log(typeof(contract))
+
+  }
+
+  async function setBaseLine(){
+    await contract.methods.setBaseLine(customerAddress,weight1,weight2,weight3)
+      .send({'from':account})
+        .then(console.log("successfully set base line"))
+  }
+
+  async function getNewData(){
+    await contract.methods.UpdateOracleData(customerAddress,weight1,weight2,weight3)
+      .send({'from':account})
+        .then(console.log("successfully set base line"))
+  }
+
+  async function settleAgreement(){
+    await contract.methods.settleAgreement(customerAddress)
+    .send({'from':account})
+      .then(console.log("successfully set base line"))
+
+  }
   
-  await contract.methods.addNewCustomer(customerAddress,donorAddress,agreementValue).send({'from':account}).then(console.log("success"))
+
+  return (
   
-  console.log(customerAddress)
-  console.log(donorAddress)
-  console.log(agreementValue)
-  console.log(typeof(contract))
+    <div 
+    className="App" 
+    style={{ backgroundImage: 'url(' + background + ')',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100vw',
+    height: '100vh',
+    }}>
+    
+    <h1>BrightLink </h1>
 
-}
+    {<button onClick={loadBlockChain} > Connect Wallet</button>}
+    <li style={{ color: 'green'}}>
+        {connected? <p><b>connected</b></p> : null}
+      </li>
 
-return (
-  <div className="App">
-  <header className="App-header"></header>
-  
-  <br></br>
-  {<button onClick={loadBlockchainData}>Connect Wallet</button>}
-  <br></br>
-  {connected ? <p>wallet connected</p>: <p></p>}
-  <br></br>
+   <p>BrightLink is deployed at: {contractAddress}</p>
+   <p>Connected to: {account}</p>
+    <br></br>
+    <br></br>
 
-  <input onChange={e => setCustomerAddress(e.target.value)} placeholder="Set Customer Address" />
-  <input onChange={e => setDonorAddress(e.target.value)} placeholder="Set Donor Address" />
-  <input onChange={e => setAgreementValue(e.target.value)} placeholder="Set Agreement Value" />
+    <p><b>Set new agreement</b></p>
+    <p><i>(you can only have one active agreement at a time)</i></p>
+    <input 
+      type="text"
+      value={customerAddress}
+      placeholder="Set Customer Address"
+      onChange={e => setCustomerAddress(e.target.value)} />
 
-  <br />
-  {<button onClick={AddNewCustomer}>Start New Agreement</button>}
+    <input 
+        type="text"
+        value={donorAddress}
+        placeholder="Set Donor Address"
+        onChange={e => setDonorAddress(e.target.value)} />
 
-</div>
-);
-}
+    <input
+      type="text"
+      value={agreementValue}
+      placeholder="Set Agreement Value"
+      onChange={e => setAgreementValue(e.target.value)} />
+
+    {<button onClick={AddNewCustomer}>Start New Agreement</button>}
+    
+    <br></br>
+    <br></br>
+
+    <p><b>Change satellite weightings and query remote sensing data</b></p>
+    <p><i>(please send 0.6 LINK to the contract to fund these queries</i></p>
+    <input
+      type="text"
+      value={weight1}
+      placeholder="Set weight for Sentinel (%)"
+      onChange={e => setWeight1(e.target.value)} />
+
+    <input
+      type="text"
+      value={weight2}
+      placeholder="Set weight for Landsat (%)"
+      onChange={e => setWeight2(e.target.value)} />
+
+    <input
+      type="text"
+      value={weight3}
+      placeholder="Set weight for MODIS (%)"
+      onChange={e => setWeight3(e.target.value)} />
+
+
+    {<button onClick={setBaseLine}>Set Baseline</button>}
+    {<button onClick={getNewData}>Get Updated Data</button>}
+
+    <br></br>
+    <br></br>
+    {<button onClick={settleAgreement}>Request Payout</button>}
+    
+    </div>
+
+  );
+  }
 
 
 export default App
---
+++ client/src/App2.js
@@ -0,0 +1,139 @@
+import React, {useState} from 'react';
+import "./App.css";
+import Web3 from 'web3'
+import background from "./logo.svg";
+import SolidityStorage from './artifacts/contracts/SolidityStorage.json';
+import map from "./artifacts/deployments/map.json"
+
+
+// uses functional component syntax
+function App(){
+
+  // web3 is whatever is injected by metamask, else localhost
+  // add deployed address for Solidity Storage contract
+  const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+  // define vars and functions to update them, associated to react state
+  
+  const [account, setOwnerAccount] = useState()
+  const [currentSolidityValue, getSolidityValue] = useState()
+  const [newSolidityValue, setNewSolidityValue] = useState()
+  const [chainID, setChainID] = useState()
+  const [network, setNetwork] = useState()
+  const [contractAddress, setContractAddress] = useState()
+  const [contract, setContract] = useState()
+  var connected = false;
+
+  async function loadBlockChain(){
+    
+    const chain = await web3.eth.getChainId()
+    const net = await web3.eth.net.getNetworkType()
+    const accounts = await web3.eth.getAccounts()
+    const acc = accounts[0]
+
+    setNetwork(net)
+    setChainID(chain)
+    setOwnerAccount(acc)
+
+    console.log(chain)
+    var address = ''
+
+    if (chain =='1337'){
+      address = map["dev"]['SolidityStorage'][0]
+    }
+    else if(chain == 42){
+      address = map["kovan"]['SolidityStorage'][0]
+    }
+    else if(chain==3){
+      address = map["ropsten"]['SolidityStorage'][0]
+    }
+    else if(chain==4){
+       address = map["rinkeby"]['SolidityStorage'][0]
+    }
+    else{
+      throw 'Please connect to a valid testnet'
+    }
+
+    setContractAddress(address)
+    const _contract = new web3.eth.Contract(SolidityStorage.abi, address, account)
+
+    setContract(_contract)
+    console.log(address)
+    console.log(_contract)
+    console.log(chain)
+    console.log("contract successfully loaded")
+  }
+
+   function checkVals(){
+     console.log(chainID)
+     console.log(contractAddress)
+     console.log(contract)
+   }
+
+  // two async functions that interact with the deployed contract:
+  // GETTER
+  async function getValue(){
+
+    let SolidityValue = await contract.methods.get().call()
+    getSolidityValue(SolidityValue)
+  }
+
+
+  //SETTER
+  async function setValue(){
+    // instantiate contract
+
+    // async call to contract's set() function
+    await contract.methods.set(newSolidityValue).send({from: account})//.then(console.log("successfully set value"))
+
+  }
+
+
+// Now define how to render in the browser
+return (
+  
+  <div 
+  className="App" 
+  style={{ backgroundImage: 'url(' + background + ')',
+  backgroundPosition: '',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  width: '100vw',
+  height: '100vh',
+  }}>
+  
+  <h1>BROWNIE-REACT-MIX </h1>
+  <h2>React front-end template</h2>
+
+  <br></br>
+  
+  {<button onClick={loadBlockChain} > Connect Wallet</button>}
+  <br></br>
+  <br></br>
+  <br></br>
+  {<button onClick={getValue}>Get Value From Contract</button>}
+  <br></br>
+  <br></br>
+
+  <input 
+      type="text"
+      placeholder="Enter new value here"
+      onChange={e => setNewSolidityValue(e.target.value)} />
+
+
+  {<button onClick={setValue}>Set New Value </button>}
+
+  <p>The stored value is: </p>
+  {currentSolidityValue}
+
+
+
+  <br></br>
+  <br></br>
+
+  </div>
+
+);
+}
+
+
+export default App
